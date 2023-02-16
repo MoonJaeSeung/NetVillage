16da80b50fb2cda94fa9976a276c5d93d9bb8cb4
임다인 <sayou1214@gmail.com>
2023-02-16 16:41:04 +0900

    Initial commit

---
+++ NetVillage/src/main/java/com/NetVillage/NetVillage/NetVillageApplication.java
@@ -0,0 +1,14 @@
+package com.NetVillage.NetVillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetVillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetVillageApplication.class, args);
+	}
+
+}
+//js
