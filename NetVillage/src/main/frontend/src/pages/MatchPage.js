5d47f0d7a24d560bc73e663c85bb7d74fc42ad36
MoonJaeSeung <wotmdskfk32@naver.com>
2023-02-21 15:54:49 +0900

    운동 친구 찾기 페이지 시작

---
+++ NetVillage/src/main/frontend/src/pages/MatchPage.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import media from "styled-media-query";
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { getMonth, getDate } from "date-fns";
+
+
+
+
+const MatchPage = () => {
+    return (
+        <div>MatchPage</div>
+    )
+}
+
+export default SchedulePage
