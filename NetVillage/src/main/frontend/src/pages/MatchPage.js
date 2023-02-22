78ee8e30bd2999c0ba6bc100e97fb9b571fab6f4
MoonJaeSeung <wotmdskfk32@naver.com>
2023-02-22 10:36:18 +0900

    운동 친구 찾기 페이지 수정

--- NetVillage/src/main/frontend/src/pages/MatchPage.js
+++ NetVillage/src/main/frontend/src/pages/MatchPage.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import media from "styled-media-query";
-import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
-import { getMonth, getDate } from "date-fns";
+import Quill from 'quill';
+import 'quill/dist/quill.bubble.css'
+import Category from "./Category";
+import './Editor.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'
+import moment from 'moment';
 
 
 
