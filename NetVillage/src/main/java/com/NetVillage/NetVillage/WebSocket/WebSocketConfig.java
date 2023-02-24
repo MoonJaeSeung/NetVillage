f0e4e8bc40ed3a589fc7de2dbed7f190838ecbd1
ignaciocha <oesnahn@gmail.com>
2023-02-24 15:37:31 +0900

    Handler 수정중

--- NetVillage/src/main/java/com/NetVillage/NetVillage/WebSocket/WebSocketConfig.java
+++ NetVillage/src/main/java/com/NetVillage/NetVillage/WebSocket/WebSocketConfig.java
@@ -1,15 +1,16 @@
 package com.NetVillage.NetVillage.WebSocket;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@Component
-public class WebSocketConfig {
-        @Bean
-        public ServerEndpointExporter serverEndpointExporter() {
-            return new ServerEndpointExporter();
-
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new EchoHandler(), "/socket/Chat");
     }
 }
