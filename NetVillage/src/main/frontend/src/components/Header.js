5835fbdaa41114354ddb9f3c1a5962be867d8193
js4183 <artomes@naver.com>
2023-02-26 19:13:58 +0900

    로그인 세션스토리지 추가 헤더 완성 로고 정하기전 커밋

--- NetVillage/src/main/frontend/src/components/Header.js
+++ NetVillage/src/main/frontend/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import styled from "styled-components";
 import media from "styled-media-query";
 import "../App.css";
+import logo from "../img/Logo1.png";
 
 const StyledHeader = styled.header`
   background-color: white;
@@ -27,7 +28,7 @@ const StyledHeader = styled.header`
 const HeaderBtn = styled(NavLink)`
   width: 100px;
   border-radius: 5px;
-  border: solid 2px #C9FFBF;
+  border: solid 2px #CAFFBE;
   box-sizing: border-box;
   text-decoration: none;
   display: inline-block;
@@ -40,24 +41,58 @@ const HeaderBtn = styled(NavLink)`
   font-family: 'GangwonEduSaeeum_OTFMediumA', serif;
   font-size: 24px;
   color: #666666;
-  
+`;
+
+const LogOutBtn = styled.button`
+  width: 100px;
+  border-radius: 5px;
+  border: solid 2px #CAFFBE;
+  box-sizing: border-box;
+  text-decoration: none;
+  display: inline-block;
+  vertical-align: middle;
+  text-align: center;
+  padding: 7px;
+  height: 40px;
+  margin: 0px 5px;
+  background-color: #ffffff;
+  font-family: 'GangwonEduSaeeum_OTFMediumA', serif;
+  font-size: 24px;
+  color: #666666;
+`;
+
+const LogoLink = styled.img`
+  height: 120px;
+  //width: 120px;
 `;
 
 const Header = () => {
+    const LogOut = () =>{
+        sessionStorage.removeItem('user_info');
+        window.location.replace("/");
+    }
     return (
         <StyledHeader>
-            <div className='Logo'>어쩌다 짝꿍</div>
-            <div className='IsLogin'>
-                <HeaderBtn to="/">홈</HeaderBtn>
-                <HeaderBtn to="/Chat">채팅페이지</HeaderBtn>
-                <HeaderBtn to="/Schedule">일정페이지</HeaderBtn>
-                <HeaderBtn to="/MyPage">마이페이지</HeaderBtn>
-                <HeaderBtn to="/Match">매치페이지</HeaderBtn>
-            </div>
-            <div className='NoIsLogin'>
+            <LogoLink
+                src={logo}
+            />
+            {sessionStorage.user_info !=null?(
+                <div className='IsLogin1'>
+                    <HeaderBtn to="/">홈</HeaderBtn>
+                    <HeaderBtn to="/Chat">채팅페이지</HeaderBtn>
+                    <HeaderBtn to="/Schedule">일정페이지</HeaderBtn>
+                    <HeaderBtn to="/Match">매치페이지</HeaderBtn>
+                </div>):
+                (<div></div>)}
+            {sessionStorage.user_info !=null?(
+                <div className='IsLogin2'>
+                    <HeaderBtn to="/MyPage">마이페이지</HeaderBtn>
+                    <HeaderBtn onClick={LogOut}>로그아웃</HeaderBtn>
+                </div>):
+                (<div className='NoIsLogin'>
                 <HeaderBtn to="/SignIn">로그인</HeaderBtn>
                 <HeaderBtn to="/Sign">회원가입</HeaderBtn>
-            </div>
+                </div>)}
         </StyledHeader>
     )
 }
--- NetVillage/src/main/frontend/src/components/User/Sign.js
+++ NetVillage/src/main/frontend/src/components/User/Sign.js
@@ -1,6 +1,76 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import styled from "styled-components";
+
+const SignContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 600px;
+  margin: 0px auto;
+  margin-top: 100px;
+  border: 2px solid #CAFFBE;
+  border-radius: 20px;
+  padding: 70px 50px 70px 50px;
+`;
+
+const P = styled.p`
+  font-size: 20px;
+  margin: 0px;
+  margin-bottom: 10px;
+`;
+
+const Input = styled.input`
+  width: 500px;
+  height: 40px;
+  margin: 0px 0px 10px 0px;
+`;
+
+const InputContainer =styled.div`
+  display: flex;
+  flexDirection: row;
+  justifyContent: center;
+`;
+
+const InputContainerLine =styled.div`
+  height: 40px;
+  display: flex;
+  flexDirection: row;
+  justifyContent: center;
+`;
+
+const CheckInput = styled.input`
+  width: 350px;
+  height: 40px;
+  margin: 0px 0px 10px 0px;
+`;
+
+const CheckButton = styled.button`
+  width: 150px;
+  border-radius: 5px;
+  border: solid 2px #CAFFBE;
+  box-sizing: border-box;
+  text-decoration: none;
+  display: inline-block;
+  vertical-align: middle;
+  text-align: center;
+  padding: 7px;
+  height: 40px;
+  margin: 0px 5px;
+  background-color: #ffffff;
+  font-family: 'GangwonEduSaeeum_OTFMediumA', serif;
+  font-size: 24px;
+  color: #666666;
+`;
+
+const Button = styled.button`
+  width: 500px;
+  height: 45px;
+  margin: 10px 5px 10px 5px;
+  background-color: #CAFFBE;
+  border: 0px solid #CAFFBE;
+  border-radius: 5px;
+`;
 
 const Sign = () => {
   const navigate = useNavigate();
@@ -142,17 +212,12 @@ const Sign = () => {
   // }
 
   return (
-    <div>
-      <main className="signup">
-        <h1 className="title">SIGN UP</h1>
-        <title className="information">
-          <h2>기본정보</h2>
-          <div>*필수입력사항</div>
-        </title>
-
+    <SignContainer>
+      <h1>회원가입을 해주세요.</h1>
+      <P>아이디</P>
+          <InputContainer>
         <form className="inputLine">
-          <div className="inputTitle">ID</div>
-          <input
+          <CheckInput
             type="text"
             className="userInput"
             onChange={handleInput}
@@ -161,11 +226,11 @@ const Sign = () => {
           />
           <div className="inputDescription">{idAlertSentence}</div>
         </form>
-        <button onClick={checkId}>아이디 중복확인</button>
-
+          <CheckButton onClick={checkId}>아이디 중복확인</CheckButton>
+          </InputContainer>
+      <P>비밀번호</P>
         <form className="inputLine">
-          <div className="inputTitle">Password</div>
-          <input
+          <Input
             type="password"
             className="userInput"
             onChange={handleInput}
@@ -173,10 +238,9 @@ const Sign = () => {
           />
           <div className="inputDescription">(영문 대소문자/숫자 4자~16자)</div>
         </form>
-
+      <P>비밀번호 확인</P>
         <form className="inputLine">
-          <div className="inputTitle">Password Check</div>
-          <input
+          <Input
             type="password"
             className="userInput"
             onChange={handleInput}
@@ -185,19 +249,18 @@ const Sign = () => {
           />
           <div className="inputDescription">{pwAlertSentence}</div>
         </form>
-
+      <P>이름</P>
         <form className="inputLine">
-          <div className="inputTitle">Name</div>
-          <input
+          <Input
             type="text"
             className="userInput"
             onChange={handleInput}
             name="name"
           />
         </form>
-
+      <P>휴대폰</P>
         <form className="phoneLine">
-          <div className="phone">Phone</div>
+          <InputContainerLine>
           <select name="phone" className="phoneSelect">
             <option>010</option>
             <option>011</option>
@@ -206,55 +269,67 @@ const Sign = () => {
             <option>018</option>
             <option>019</option>
           </select>
-          <input
+          <Input
             type="text"
             className="phoneSecond"
             onChange={handleInput}
             onBlur={() => phoneCheck(phone)}
             name="phone"
           />
+          </InputContainerLine>
           <div className="phoneDescription">{phoneAlertSentence}</div>
         </form>
-
         <form className="cityLine">
-          <div className="city">city</div>
+          <InputContainerLine>
+          <P>지역을 선택해주세요.</P>
+            <div>
           <select name="city" className="citySelect" onChange={handleInput}>
+            <option>선택해주세요.</option>
             <option value="광산구">광산구</option>
             <option value="북구">북구</option>
             <option value="동구">동구</option>
             <option value="남구">남구</option>
             <option value="서구">서구</option>
           </select>
-        </form>
-
-        <form className="regionLine">
-          <div className="region">region</div>
+            </div>
+            <div>
           <select name="region" className="regionSelect" onChange={handleInput}>
+            <option>선택해주세요.</option>
             <option>월계동</option>
             <option>신창동</option>
           </select>
+            </div>
+          </InputContainerLine>
         </form>
 
         <form className="genderLine">
-          <div className="gender">gender</div>
+          <InputContainerLine>
+          <P>성별을 선택해주세요.</P>
+            <div>
           <label for="male">남성</label>
           <input id="male" type="radio" value="M" name="gender" onChange={handleInput}/>
+            </div>
+            <div>
           <label for="female">여성</label>
           <input id="female" type="radio" value="F" name="gender" onChange={handleInput}/>
+            </div>
+          </InputContainerLine>
         </form>
-
+      <P>닉네임</P>
+          <InputContainer>
         <form className="inputLine">
-          <div className="inputTitle">Nick</div>
-          <input
+          <CheckInput
             type="text"
             className="nickInput"
             onChange={handleInput}
             name="nick"
           />
         </form>
-          <button onClick={checkNick}>닉네임 중복확인</button>
+          <CheckButton onClick={checkNick}>닉네임 중복확인</CheckButton>
+          </InputContainer>
 
-        <button
+
+        <Button
           onClick={addUser}
           className="signupBtn"
           disabled={
@@ -267,9 +342,8 @@ const Sign = () => {
           }
         >
           회원가입
-        </button>
-      </main>
-    </div>
+        </Button>
+    </SignContainer>
   )
 }
 
