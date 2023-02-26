17c12b19dcf52bf730595844d6fc288a125445a7
moonjaeseong <wotmdskfk32@naver.com>
2023-02-26 12:28:53 +0900

    운동 친구 찾기 페이지 구성

--- NetVillage/src/main/frontend/src/App.js
+++ NetVillage/src/main/frontend/src/App.js
@@ -10,6 +10,9 @@ import Intro from './pages/Intro';
 import './App.css'
 import Sign from './components/User/Sign'
 import Footer from './components/Footer';
+import MatchPage from "./pages/MatchPage";
+import WriteMatchPage from "./pages/WriteMatchPage";
+import MatchHeader from "./components/Match/MatchHeader";
 
 
 function App() {
@@ -18,32 +21,36 @@ function App() {
 
     //유저정보가 없으면 인트로 화면 있으면 메인 화면
     const loginUser = () => {
-        if(user == null){
+        if (user == null) {
             return <MainPage/>
             // return <Intro/> 로그인 정보 담으면 주석 풀어서 실행하기
-        }else{
+        } else {
             return <MainPage/>
         }
 
     }
 
-  return (
-      <div>
-        <Header/>
-          {/*{loginUser()}*/}
-        {/* 로그인 정보 저장하면 아래 메인 페이지 지우기, root경로 인트로 페이지로 바꾸기 */}
-        <Routes>
-          <Route path="/" element={<MainPage/>}/>
-          <Route path="/Intro" element={<Intro/>}/>
-          <Route path="/Chat" element={<ChatPage/>}/>
-          <Route path="/Schedule" element={<SchedulePage/>}/>
-          <Route path="/MyPage" element={<MyPage/>}/>
-          <Route path="/SignIn" element={<SignIn/>}/>
-          <Route path="/Sign" element={<Sign/>}/>
-        </Routes>
-        <Footer/>
-      </div>
-  );
+    return (
+        <div>
+            <Header/>
+            {/*{loginUser()}*/}
+            {/* 로그인 정보 저장하면 아래 메인 페이지 지우기, root경로 인트로 페이지로 바꾸기 */}
+            <Routes>
+                <Route path="/" element={<MainPage/>}/>
+                <Route path="/Intro" element={<Intro/>}/>
+                <Route path="/Chat" element={<ChatPage/>}/>
+                <Route path="/Schedule" element={<SchedulePage/>}/>
+                <Route path="/MyPage" element={<MyPage/>}/>
+                <Route path="/SignIn" element={<SignIn/>}/>
+                <Route path="/Sign" element={<Sign/>}/>
+                <Route path="/Match" element={<MatchHeader/>}>
+                    <Route index element={<MatchPage/>}/>
+                    <Route path="Write" element={<WriteMatchPage/>}/>
+                </Route>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ NetVillage/src/main/frontend/src/components/Match/MatchRoomCard.js
@@ -0,0 +1,27 @@
+const MatchRoomCard = (props) => {
+
+    return (
+        <div style={{background : "grey"}}>
+            <p>1월 27일 저녁</p>
+            <div>
+                <div>아이콘</div>
+                <div>VS or With</div>
+                <div>글쓴이</div>
+            </div>
+            { props.state
+                ? <small>10승 2패</small>
+                : <p>코멘트</p>
+            }
+            <div>
+                <small>D-7</small>
+                <p>장소</p>
+                <div>
+                    <button>수정</button>
+                    <button>삭제</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MatchRoomCard;
