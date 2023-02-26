b462254073d2861527bc8b50a354c0e3258530a7
js4183 <artomes@naver.com>
2023-02-26 15:26:22 +0900

    헤더 수정중

--- NetVillage/src/main/frontend/src/components/Header.js
+++ NetVillage/src/main/frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styled from "styled-components";
 import media from "styled-media-query";
 import "../App.css";
@@ -24,7 +24,7 @@ const StyledHeader = styled.header`
   `}
 `;
 
-const HeaderBtn = styled.button`
+const HeaderBtn = styled(NavLink)`
   width: 100px;
   border-radius: 5px;
   border: solid 2px #C9FFBF;
@@ -40,50 +40,23 @@ const HeaderBtn = styled.button`
   font-family: 'GangwonEduSaeeum_OTFMediumA', serif;
   font-size: 24px;
   color: #666666;
+  
 `;
 
 const Header = () => {
-    const Navigate = useNavigate();
-
-    const toMainPage = () => {
-        Navigate("/")
-    }
-
-    const toChatPage = () => {
-        Navigate("/Chat")
-    }
-
-    const toSchedulePage = () => {
-        Navigate("/Schedule")
-    }
-
-    const toMyPage = () => {
-        Navigate("/MyPage")
-    }
-
-    const toSignIn = () => {
-        Navigate("/SignIn")
-    }
-    const toSign = () => {
-        Navigate("/Sign")
-    }
-
-    const toMatchPage = () => {
-        Navigate("/Match")
-    }
     return (
         <StyledHeader>
             <div className='Logo'>어쩌다 짝꿍</div>
             <div className='IsLogin'>
-                <button className='HomeBtn' onClick={toMainPage}>홈</button>
-                <button className='ChatBtn' onClick={toChatPage}>채팅</button>
-                <button className='CalendarBtn' onClick={toSchedulePage}>일정</button>
-                <button className='MyPageBtn' onClick={toMyPage}>마이페이지</button>
-                <button className='MyPageBtn' onClick={toMatchPage}>매치페이지</button>
+                <HeaderBtn to="/">홈</HeaderBtn>
+                <HeaderBtn to="/Chat">채팅페이지</HeaderBtn>
+                <HeaderBtn to="/Schedule">일정페이지</HeaderBtn>
+                <HeaderBtn to="/MyPage">마이페이지</HeaderBtn>
+                <HeaderBtn to="/Match">매치페이지</HeaderBtn>
             </div>
             <div className='NoIsLogin'>
-                <button className='LoginBtn' onClick={toSignIn}>로그인</button>
-                <button className='RegisterBtn' onClick={toSign}>회원가입</button>
+                <HeaderBtn to="/SignIn">로그인</HeaderBtn>
+                <HeaderBtn to="/Sign">회원가입</HeaderBtn>
             </div>
         </StyledHeader>
     )
