abc11cfe56f0d811ec44171dc52b7427fda76135
MoonJaeSeung <wotmdskfk32@naver.com>
2023-02-27 10:45:19 +0900

    친구 찾기 페이지 CSS 수정

--- NetVillage/src/main/frontend/src/pages/WriteMatchPage.js
+++ NetVillage/src/main/frontend/src/pages/WriteMatchPage.js
@@ -3,33 +3,78 @@ import styled from "styled-components";
 
 const WriteMatchPageWrapper = styled.div`
   color: blue;
+  padding: 20px;
+  padding-left: 30px;
 `
 
 const WriteMatchHeader = styled.div`
-  color: brown;
+  color: black;
+  padding-bottom: 30px;
 `
 
 const WriteMatchBody = styled.div`
   color: black;
 `
+const Title = styled.input`
+    width: 91%;
+    padding-bottom: 20px;
+    border: none;
+    font-size: 22px;
+    border-bottom: solid 1px springgreen;
+    font-weight: bold;
+`
+
+const CategoryBox = () =>{
+    return(
+        <select>
+            <option key="free" value="free">
+                자유
+            </option>
+            <option key="vs" value="vs">
+                대결
+            </option>
+            <option key="mento" value="mento">
+                멘토 찾기
+            </option>
+            }
+        </select>
+    )
+}
+
+const SportBox = () =>{
+    return(
+        <select>
+            <option key="free" value="free">
+                탁구
+            </option>
+            <option key="vs" value="vs">
+                테니스
+            </option>
+            <option key="mento" value="mento">
+                볼링
+            </option>
+            }
+        </select>
+    )
+}
+
 
 const WriteMatchPage = () => {
     return (<WriteMatchPageWrapper>
         <WriteMatchHeader>
             <h2>글쓰기</h2>
-            <div>
-                게시판 선택 드롭다운
-            </div>
-            <input type="text" />
+            <Title type="text"  placeholder='제목'></Title>
         </WriteMatchHeader>
         <WriteMatchBody>
+
+
             <div>
                 <small>카테고리</small>
-                <div>드롭다운</div>
+                <div><CategoryBox></CategoryBox></div>
             </div>
             <div>
                 <small>종목</small>
-                <div>드롭다운</div>
+                <div><SportBox></SportBox></div>
             </div>
             <div>
                 <small>날짜</small>
