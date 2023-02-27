a57e96eabf153f7019b5ef3057fa79deecb2a618
MoonJaeSeung <wotmdskfk32@naver.com>
2023-02-27 16:46:37 +0900

    친구 찾기 페이지 CSS 수정 5차

--- NetVillage/src/main/frontend/src/components/Match/MatchHeader.js
+++ NetVillage/src/main/frontend/src/components/Match/MatchHeader.js
@@ -1,28 +1,86 @@
 import {Outlet} from "react-router-dom";
 import React from "react";
+import styled from "styled-components";
+
+
+const H=styled.div`
+  display:"flex";
+  justify-content: "center";
+`
+
+const SportBox = () =>{
+    return(
+        <select>
+            <option key="free" value="free">
+                탁구
+            </option>
+            <option key="vs" value="vs">
+                볼링
+            </option>
+            <option key="mento" value="mento">
+                테니스
+            </option>
+            }
+        </select>
+    )
+}
+
+const RecruitBox = () =>{
+    return(
+        <select>
+            <option key="free" value="free">
+                모집 중
+            </option>
+            <option key="vs" value="vs">
+                모집 완료
+            </option>
+            }
+        </select>
+    )
+}
+
+const FilterBox = () =>{
+    return(
+        <select>
+            <option key="free" value="free">
+                작성자 이름
+            </option>
+            <option key="vs" value="vs">
+                작성자 ID
+            </option>
+            <option key="vs" value="vs">
+                경기 장소
+            </option>
+            }
+        </select>
+    )
+}
 
 const MatchHeader = () => {
     return(
+
         <div>
-            <div>
+            <span>
+                <small>위치</small>
+                <span style={{fontSize:"30px"}}> ►  동명동</span>
+                <button>지도로 보기</button>
+            </span>
+
+            <span style={{display:"flex",justifyContent:"center", alignContent:"center"}}>
+                <SportBox></SportBox>
+                <RecruitBox></RecruitBox>
+                <FilterBox></FilterBox>
+                <input type="text"/>
+                <button>검색</button>
+            </span>
+
+
                 <header>
-                    <div>
-                        <small>동네인증</small>
-                        <div>동명동</div>
-                        <button>지도로 보기</button>
-                    </div>
-                    <div>
-                        <div>탁구 드롭다운</div>
-                        <div>모집 드롭다운</div>
-                        <div>작성자이름 드롭다운</div>
-                        <input type="text"/>
-                        <button>검색</button>
-                    </div>
                 </header>
                 <main>
                     <Outlet/>
                 </main>
-            </div>
+
         </div>
     );
 }
--- NetVillage/src/main/frontend/src/components/Match/MatchRoomCard.js
+++ NetVillage/src/main/frontend/src/components/Match/MatchRoomCard.js
@@ -5,12 +5,12 @@ const MatchRoomCard = (props) => {
     }
 
     return (
-        <div style={{background : "rgb(233, 255, 228)", width: "400px", height: "300px"}}>
+        <div style={{background : "rgb(233, 255, 228)", width: "30%", height:"20%",marginBottom:"15px",borderRadius:"20px"}}>
             <p style={date}>1월 27일 저녁</p>
-            <div>
+            <div style={{display:"flex",justifyContent:"center"}}>
                 <img src="https://o.remove.bg/downloads/5b21b4b7-569d-4149-940e-e6aca5f027f5/softee-%E1%84%90%E1%85%A1%E1%86%A8%E1%84%80%E1%85%AE-%E1%84%85%E1%85%A1%E1%84%8F%E1%85%A6%E1%86%BA-p-300-removebg-preview.png" style={{width:"50px"}}></img>
-                <div>VS or With</div>
-                <div>글쓴이</div>
+                <span>VS or With</span>
+                <div style={{marginLeft:"5px"}}>글쓴이</div>
             </div>
             { props.state
                 ? <small>10승 2패</small>
