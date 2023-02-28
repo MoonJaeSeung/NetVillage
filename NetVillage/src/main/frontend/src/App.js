9e60723a7ce973947f5c66c15d397f87ff162526
ignaciocha <oesnahn@gmail.com>
2023-02-28 16:16:29 +0900

    ChatController 수정중

--- NetVillage/src/main/frontend/src/App.js
+++ NetVillage/src/main/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Routes, Route} from "react-router-dom";
 import MainPage from './pages/MainPage';
 import Header from './components/Header';
