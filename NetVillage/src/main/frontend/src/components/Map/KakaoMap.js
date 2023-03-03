f357d55f301a2a008993f4c1ca431fc5e1bdb94a
js4183 <artomes@naver.com>
2023-03-03 15:52:22 +0900

    지도 수정

--- NetVillage/src/main/frontend/src/components/Map/KakaoMap.js
+++ NetVillage/src/main/frontend/src/components/Map/KakaoMap.js
@@ -1,24 +1,69 @@
 /* global kakao */
-import React, { useEffect } from "react";
+import React, { useState,useEffect } from "react";
 
 const { kakao } = window;
 
 const KakaoMap = () => {
+    const [currentPosition, setCurrentPosition] = useState(null);
+
     useEffect(() => {
-        const container = document.getElementById("map");
-
-        const options = {
-            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-            level: 3,
-        };
-
-        const map = new window.kakao.maps.Map(container, options);
-
+        // 위치 정보 가져오기
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                setCurrentPosition({ latitude, longitude });
+            },
+            (error) => {
+                console.error(error);
+            }
+        );
     }, []);
+    useEffect(() => {
+        if (currentPosition) {
+            // 카카오 맵 초기화
+            const container = document.getElementById("map");
+            const options = {
+                center: new window.kakao.maps.LatLng(
+                    currentPosition.latitude,
+                    currentPosition.longitude
+                ),
+                level: 3,
+            };
+            const map = new window.kakao.maps.Map(container, options);
 
-    return (
-        <div id="map" style={{width:'500px',height:'500px'}}></div>
-    );
+            // 마커 생성
+            const marker = new window.kakao.maps.Marker({
+                position: new window.kakao.maps.LatLng(
+                    currentPosition.latitude,
+                    currentPosition.longitude
+                ),
+            });
+
+            // 지도에 마커 추가
+            marker.setMap(map);
+
+            // 지오코딩을 위한 객체 생성
+            const geocoder = new window.kakao.maps.services.Geocoder();
+
+            // 좌표를 주소로 변환하여 출력하는 함수
+            const displayAddress = (result, status) => {
+                if (status === window.kakao.maps.services.Status.OK) {
+                    console.log(result[0].address.address_name);
+                } else {
+                    console.error("Error");
+                }
+            };
+
+            // 좌표를 주소로 변환
+            geocoder.coord2RegionCode(
+                currentPosition.longitude,
+                currentPosition.latitude,
+                displayAddress
+            );
+        }
+    }, [currentPosition]);
+
+    return <div id="map" style={{ width: '500px', height: '500px' }} />;
 };
 
 export default KakaoMap;
